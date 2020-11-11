7e1e198e5d20c92ef7a026931bb5fcf46feb1e52
MaximMyDev <max.monarch@gmail.com>
2020-11-11 18:57:18 +0600

    fix del and add

--- src/main/java/web/controller/RestTest.java
+++ src/main/java/web/controller/RestTest.java
@@ -3,23 +3,30 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import web.model.Role;
 import web.model.User;
 import web.service.UserServiceImpl;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/adminrest")
 public class RestTest {
 
     private final UserServiceImpl userService;
-    @Autowired
-    public RestTest(UserServiceImpl userService) {
-        this.userService = userService;
-    }
+    private final PasswordEncoder passwordEncoder;
 
+    @Autowired
+    public RestTest(UserServiceImpl userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+    //вывод всех
     @GetMapping(value = "/list")
     public ResponseEntity<List<User>> getAllUsers() {
         final List<User> userList = userService.allUsers();
@@ -28,15 +35,24 @@ public class RestTest {
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    //добавление нового
     @PostMapping(value = "/add")
-    public ResponseEntity<Object> addNew(@RequestBody User user, @RequestParam("role") String[] role) {
-        System.out.println(user.getName());
-        System.out.println(role.toString());
+    public ResponseEntity<Object> addNew(@RequestBody User user,
+                                         @RequestParam(value = "roles", required = false) String[] roles) {
+        Set<Role> rolesArray = new HashSet<>();
+        for (String role : roles) {
+            Optional<Role> currentRole = userService.getRoleByRoleName(role);
+            rolesArray.add(currentRole.get());
+        }
+        user.setRoles(rolesArray);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         userService.add(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/edit/{id}")
+    //получение одного юзера
+    @GetMapping(value = "/user/{id}")
     public ResponseEntity<Optional<User>> read(@PathVariable(name = "id") Long id) {
         final Optional<User> client = userService.getById(id);
         return client != null
@@ -44,15 +60,7 @@ public class RestTest {
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    /*@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
-    public ModelAndView deleteUser(@PathVariable("id") Long id) {
-        Optional<User> user = userService.getById(id);
-        userService.delete(user.get());
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/admin/list");
-        return modelAndView;
-    }*/
-
+    //удаление юзера
     @GetMapping("/delete/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable("id") Long id) {
         Optional<User> user = userService.getById(id);
@@ -60,11 +68,37 @@ public class RestTest {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping(value = "/user/{id}")
-    public ResponseEntity<Optional<User>> getOneUser(@PathVariable(name = "id") Long id) {
-        final Optional<User> client = userService.getById(id);
-        return client != null
-                ? new ResponseEntity<>(client, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    //обновление юзера
+    @PostMapping(value = "/edit")
+    public ResponseEntity<Object> editUser(@RequestBody User user,
+                                         @RequestParam(value = "roles", required = false) String[] roles) {
+        Set<Role> rolesArray = new HashSet<>();
+        for (String role : roles) {
+            Optional<Role> currentRole = userService.getRoleByRoleName(role);
+            rolesArray.add(currentRole.get());
+        }
+        user.setRoles(rolesArray);
+
+        if (user.getPassword().isEmpty() || user.getPassword() == null) {
+            Optional<User> newUser = userService.getUserByName(user.getName());
+            user.setPassword(newUser.get().getPassword());
+        } else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+
+        userService.edit(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
 }
+
+
+
+//
+//    @GetMapping(value = "/user/{id}")
+//    public ResponseEntity<Optional<User>> getOneUser(@PathVariable(name = "id") Long id) {
+//        final Optional<User> client = userService.getById(id);
+//        return client != null
+//                ? new ResponseEntity<>(client, HttpStatus.OK)
+//                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,60 +1,43 @@
 $(document).ready(fillUserTable());
 
-function fillUserTable() {
-    const response = fetch('http://localhost:8089/adminrest/list')
-        .then((response) => {
-            response.json().then((data) => {
-                data.forEach(function (i) {
-                    let roles = i.authorities;
-                    let allRoles = '';
-                    let count = 0;
-                    let separator = ', ';
-                    roles.forEach(function (item) {
-                        if (count>0) {
-                            allRoles += separator + item.role;
-                        } else {
-                        allRoles += item.role;
-                        }
-                        count++;
-                    })
-                    let html = '<tr id=' + i.id + '>' +
-                        '<td>' + i.id + '</td>' +
-                        '<td>' + i.name + '</td>' +
-                        '<td>' + i.email + '</td>' +
-                        '<td>' + allRoles + '</td>' +
-                        '<td>' +
-                        '<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#editUserModal" onclick="editUser(this)">' +
-                        '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'+
-                        '<path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>'+
-                        '</svg>'+
-                        '</button>'+
-                        '</td>' +
-                        '<td>' +
-                        '<button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteUserModal" onclick="deleteRow(this)">' +
-                        '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
-                        '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>'+
-                        '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>'+
-                        '</svg>'+
-                        '</button>'+
-                        '</td>' +
-                        '</tr>';
-                    $('#usersTable').append(html);
-                });
-            });
-        });
-}
 
+function addUser() {
+    let roles = '?roles=';
+    const newRoles = $('#roleAdd').val();
+    console.log("newRoles="+newRoles);
+    newRoles.forEach(function (item) {
+        roles += item + ',';
+    })
+    console.log("roles="+roles);
+    const rolesSend = roles.substr(0, roles.length - 1);
+    let jsonVar = {
+        name: document.getElementById("nameAdd").value,
+        email: document.getElementById("emailAdd").value,
+        password: document.getElementById("passwordAdd").value
+    };
+    console.log("jsonVar="+jsonVar)
+    const response = fetch('http://localhost:8089/adminrest/add' + rolesSend, {
+        method: 'POST',
+        body: JSON.stringify(jsonVar),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    document.getElementById('table-tab').click();
+    $("#usersTable > tbody").empty();
+    fillUserTable();
+}
 
 function editUser(o) {
     document.getElementById('editUserForm').reset();
     const id = $(o).closest('tr').find('td').eq(0).text();
-    const url = 'http://localhost:8089/adminrest/edit/' + id;
+    const url = 'http://localhost:8089/adminrest/user/' + id;
     fetch(url)
         .then((response) => {
             response.json().then((data) => {
-                $('#id').val(data.id);
-                $('#name').val(data.name)
-                $('#email').val(data.email);
+                $('#idEdit').val(data.id);
+                $('#nameEdit').val(data.name)
+                $('#emailEdit').val(data.email);
 
                 const roles = data.authorities;
                 roles.forEach(function (item) {
@@ -63,6 +46,32 @@ function editUser(o) {
 
             });
         });
+}
+
+
+function updateUser() {
+    let roles = '?roles=';
+    const newRoles = $('#editFormControlSelect').val();
+    newRoles.forEach(function (item) {
+        roles += item + ',';
+    })
+    const rolesSend = roles.substr(0, roles.length - 1);
+    const jsonVar = {
+        name: document.getElementById("nameEdit").value,
+        email: document.getElementById("emailEdit").value,
+        password: document.getElementById("passwordEdit").value
+    };
+    console.log(jsonVar);
+
+    const response = fetch('http://localhost:8089/adminrest/edit' + rolesSend, {
+        method: 'POST',
+        body: JSON.stringify(jsonVar),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    $("#usersTable > tbody").empty();
+    printUsers();
 }
 
 function getUser() {
@@ -81,10 +90,8 @@ function getUser() {
                 console.log(stringRoles)
                 let html = '<tr>' +
                     '<td>' + data.id + '</td>' +
-                    '<td>' + data.firstName + '</td>' +
-                    '<td>' + data.lastName + '</td>' +
+                    '<td>' + data.name + '</td>' +
                     '<td>' + data.email + '</td>' +
-                    '<td>' + data.age + '</td>' +
                     '<td>' + stringRoles + '</td>' +
                     '</tr>';
                 $('#userInfo').append(html);
@@ -126,68 +133,53 @@ function deleteUser() {
     row.parentElement.removeChild(row);
 }
 
-
-
 function cleanForm() {
-    document.getElementById('addForm').reset();
+    document.getElementById('newUserForm').reset();
 }
 
 
-function updateUser() {
-    let roles = '?roles=';
-    const newRoles = $('#editFormControlSelect').val();
-    newRoles.forEach(function (item) {
-        roles += item + ',';
-    })
-    console.log(newRoles)
-    console.log(roles)
-    const roles1 = roles.substr(0, roles.length - 1);
-    console.log(roles1)
-    const jsonVar = {
-        id: document.getElementById("idEdit").value,
-        firstName: document.getElementById("name").value,
-        lastName: document.getElementById("email").value
-        /*age: document.getElementById("ageEdit").value,
-        email: document.getElementById("emailEdit").value,
-        password: document.getElementById("editInputPassword").value*/
-    };
-    const response = fetch('http://localhost:8089/api/admin/edit' + roles1, {
-        method: 'PUT',
-        body: JSON.stringify(jsonVar),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    alert("успешно");
-    $("#usersTable > tbody").empty();
-    printUsers();
-}
-
-function addUser() {
-    let roles = '?roles=';
-    const newRoles = $('#roleAdd').val();
-    console.log(newRoles);
-    newRoles.forEach(function (item) {
-        roles += item + ',';
-    })
-    const roles1 = roles.substr(0, roles.length - 1);
-    let jsonVar = {
-        name: document.getElementById("nameAdd").value,
-        email: document.getElementById("emailAdd").value,
-        password: document.getElementById("passwordAdd").value
-    };
-    console.log(jsonVar)
-    const response = fetch('http://localhost:8089/adminrest/add' + roles1, {
-        method: 'POST',
-        body: JSON.stringify(jsonVar),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    alert("успешно");
-    document.getElementById('v-pills-admin-tab').click();
-    $("#usersTable > tbody").empty();
-    fillUserTable();
+function fillUserTable() {
+    const response = fetch('http://localhost:8089/adminrest/list')
+        .then((response) => {
+            response.json().then((data) => {
+                data.forEach(function (i) {
+                    let roles = i.authorities;
+                    let allRoles = '';
+                    let count = 0;
+                    let separator = ', ';
+                    roles.forEach(function (item) {
+                        if (count>0) {
+                            allRoles += separator + item.role;
+                        } else {
+                            allRoles += item.role;
+                        }
+                        count++;
+                    })
+                    let html = '<tr id=' + i.id + '>' +
+                        '<td>' + i.id + '</td>' +
+                        '<td>' + i.name + '</td>' +
+                        '<td>' + i.email + '</td>' +
+                        '<td>' + allRoles + '</td>' +
+                        '<td>' +
+                        '<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#editUserModal" onclick="editUser(this)">' +
+                        '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'+
+                        '<path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>'+
+                        '</svg>'+
+                        '</button>'+
+                        '</td>' +
+                        '<td>' +
+                        '<button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteUserModal" onclick="deleteRow(this)">' +
+                        '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">' +
+                        '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>'+
+                        '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>'+
+                        '</svg>'+
+                        '</button>'+
+                        '</td>' +
+                        '</tr>';
+                    $('#usersTable').append(html);
+                });
+            });
+        });
 }
 
 let userId;
